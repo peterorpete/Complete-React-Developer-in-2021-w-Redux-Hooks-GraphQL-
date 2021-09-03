3eb693e4c408a3a3d7c69f93e25b01c601e8dcb2
Pete <peterorpete@me.com>
2021-09-03 23:45:50 +0100

    card component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,33 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import { CardList } from "./components/card-list/card-list.component";
 class App extends Component {
  constructor() {
   super();
   this.state = {
-   monsters: [
-    {
-     name: "Frankenstein",
-     id: "asc1",
-    },
-    {
-     name: "Dracula",
-     id: "asc2",
-    },
-    {
-     name: "Zombie",
-     id: "asc3",
-    },
-   ],
+   monsters: [],
   };
+ }
+ componentDidMount() {
+  fetch("http://jsonplaceholder.typicode.com/users")
+   .then((response) => response.json())
+   .then((users) => this.setState({ monsters: users }));
  }
 
  render() {
   return (
    <div className="App">
-    {this.state.monsters.map((monster) => (
-     <h1 key={monster.id}> {monster.name}</h1>
-    ))}
+    <CardList name="Yihua">
+     {this.state.monsters.map((monster) => (
+      <h1 key={monster.id}> {monster.name}</h1>
+     ))}
+    </CardList>
    </div>
   );
  }
