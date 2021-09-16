14c8277f8964794dd10f6d0c9fae39d72a07911c
Pete <peterorpete@me.com>
2021-09-16 12:08:53 +0100

    new monster set

--- monsters-rolodex/src/components/card/card.component.jsx
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import "./card.styles.css";
 
 export const Card = (props) => (
  <div className="card-container">
-  <img alt="Monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
+  <img alt="Monster" src={`https://robohash.org/${props.monster.id}?set=set1&size=180x180`} />
   <h2> {props.monster.name}</h2>
   <p>{props.monster.email}</p>
  </div>
